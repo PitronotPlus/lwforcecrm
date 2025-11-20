6d74105d9f597890d7036babb0258bd2a1e641e9
JOHN ELGORT <johnelgort@gmail.com>
2025-11-20 04:22:25 +0000

    File changes

--- src/pages/Cases.jsx
+++ src/pages/Cases.jsx
@@ -223,7 +223,6 @@ export default function Cases() {
                         </CaseModal>
                     </div>
                 )}
-                </div>
             </div>
         </div>
     );
